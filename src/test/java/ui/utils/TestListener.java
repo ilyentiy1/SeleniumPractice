8f08752f121b26796c428496f8409678a7f9834a
ilyentiy1 <ilyakamaevv13@gmail.com>
2026-03-27 15:53:16 +0300

    1.8.17

--- src/test/java/ui/utils/TestListener.java
+++ src/test/java/ui/utils/TestListener.java
@@ -14,16 +14,18 @@ import java.io.IOException;
 public class TestListener extends TestListenerAdapter {
     @Override
     public void onTestFailure(ITestResult result) {
-        Object currentClass = result.getInstance();
         WebDriver driver = BaseTest.getDriver();
 
-        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        try {
-            // Сохраняем файл в папку screenshots с уникальным именем
-            FileUtils.copyFile(srcFile, new File("src/test/resources/screenshots/" + "screenshot_"
-                    + System.currentTimeMillis() + ".png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if (driver != null) {
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(srcFile, new File("src/test/resources/screenshots/"
+                        + "screenshot_" + System.currentTimeMillis() + ".png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            System.out.println("Screenshot skip: WebDriver is null");
         }
     }
 }
