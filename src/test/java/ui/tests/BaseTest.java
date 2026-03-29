07f3219173dcdb429185096b289ddcc133c8cf75
ilyentiy1 <ilyakamaevv13@gmail.com>
2026-03-29 14:18:15 +0300

    1.8.25

--- src/test/java/ui/tests/BaseTest.java
+++ src/test/java/ui/tests/BaseTest.java
@@ -17,7 +17,7 @@ abstract public class BaseTest {
     protected static ThreadLocal<RemoteWebDriver> threadDriver = new ThreadLocal<>();
 
     @BeforeMethod(alwaysRun = true)
-    public void setUp() throws MalformedURLException {
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
 
         options.setCapability("selenoid:options", Map.of(
@@ -26,24 +26,18 @@ abstract public class BaseTest {
                 "screenResolution", "1920x1080x24"
         ));
 
-//        try {
-//            RemoteWebDriver driver = new RemoteWebDriver(
-//                    URI.create("http://host.docker.internal:4444/wd/hub").toURL(), options
-//            );
-//            threadDriver.set(driver);
-//        } catch (MalformedURLException e) {
-//            throw new RuntimeException("Ошибка в адресе Selenoid", e);
-//        }
-
-        RemoteWebDriver driver = new RemoteWebDriver(
-                URI.create("http://host.docker.internal:4444/wd/hub").toURL(), options
-        );
-        threadDriver.set(driver);
-
+        try {
+            RemoteWebDriver driver = new RemoteWebDriver(
+                    URI.create("http://host.docker.internal:4444/wd/hub").toURL(), options
+            );
+            threadDriver.set(driver);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Ошибка в адресе Selenoid", e);
+        }
 
         getDriver().manage().window().maximize();
         getDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
 
